259826c9a14318cb92ef1961ead069eba5f3f099
Jacz <itsmejacz@gmail.com>
2020-03-23 23:13:19 +1100

    All done

--- src/lib/Types.ts
+++ src/lib/Types.ts
@@ -1,6 +1,9 @@
 import { Player } from "./Player";
 import WebSocket from "ws";
 
+/**
+ * The Lavalink Event
+ * */
 export interface LavalinkEvent {
     /**
      * The type of event from lavalink
@@ -12,6 +15,9 @@ export interface LavalinkEvent {
     reason?: "FINISHED" | "LOAD_FAILED" | "STOPPED" | "REPLACED" | "CLEANUP";
 }
 
+/**
+ * Lavalink Player State
+ */
 export interface LavalinkPlayerState {
     /**
      * The current time in milliseconds
@@ -23,6 +29,9 @@ export interface LavalinkPlayerState {
     position?: number;
 }
 
+/**
+ * Player Options
+ */
 export interface PlayerOptions {
     /**
      * The id of the player, aka the guild id
@@ -34,6 +43,9 @@ export interface PlayerOptions {
     channel: string;
 }
 
+/**
+ * Player State
+ */
 export interface PlayerState extends LavalinkPlayerState {
     /**
      * The current volume of the Player, used for end user as lavalink doesn't provide this
@@ -45,6 +57,9 @@ export interface PlayerState extends LavalinkPlayerState {
     equalizer: PlayerEqualizerBand[];
 }
 
+/**
+ * Player Play Options
+ */
 export interface PlayerPlayOptions {
     /**
      * Where to start the song fromm if you wanted to start from somewhere
@@ -68,6 +83,9 @@ export interface PlayerPlayOptions {
     volume?: number;
 }
 
+/**
+ * Player Equalizer Band
+ */
 export interface PlayerEqualizerBand {
     /**
      * There are 15 bands (0-14) that can be changed
@@ -79,11 +97,17 @@ export interface PlayerEqualizerBand {
     gain: number;
 }
 
+/**
+ * Player Update Voice State
+ */
 export interface PlayerUpdateVoiceState {
     /**
      * The session id of the voice connection
      */
     sessionId: string;
+    /**
+     * Event data
+     */
     event: {
         /**
          * The token for the voice session
@@ -100,6 +124,9 @@ export interface PlayerUpdateVoiceState {
     };
 }
 
+/**
+ * Manager Options
+ */
 export interface ManagerOptions {
     /**
      * User id of the bot
@@ -118,7 +145,9 @@ export interface ManagerOptions {
      */
     send: (packet: DiscordPacket) => unknown;
 }
-
+/**
+ * Manager Join Data
+ */
 export interface JoinData {
     /**
      * The guild id of the guild the voice channel is in, that you want to join
@@ -134,75 +163,237 @@ export interface JoinData {
     node: string;
 }
 
+/**
+ * Manager Join Options
+ */
 export interface JoinOptions {
+    /**
+     * Whether or not the bot will be self muted when it joins the voice channel
+     */
     selfmute?: boolean;
+    /**
+     * Whether or not the bot will be self deafen when it joins the voice channel
+     */
     selfdeaf?: boolean;
 }
 
+/**
+ * Voice Server Update
+ */
 export interface VoiceServerUpdate {
+    /**
+     * The token for the session
+     */
     token: string;
+    /**
+     * Guild if of the voice connection
+     */
     guild_id: string;
+    /**
+     * The endpoint lavalink will connect to
+     */
     endpoint: string;
 }
 
+/**
+ * Voice State Update
+ */
 export interface VoiceStateUpdate {
+    /**
+     * Guild id
+     */
     guild_id: string;
+    /**
+     * channel id
+     */
     channel_id?: string;
+    /**
+     * User id
+     */
     user_id: string;
+    /**
+     * Session id
+     */
     session_id: string;
+    /**
+     * Whether the user is deafened or not
+     */
     deaf?: boolean;
+    /**
+     * Whether the user is muted or not
+     */
     mute?: boolean;
+    /**
+     * Whether the user is self-deafened or not
+     */
     self_deaf?: boolean;
+    /**
+     * Whether the user is self-muted or not
+     */
     self_mute?: boolean;
+    /**
+     * Whether the user is suppressed
+     */
     suppress?: boolean;
 }
 
+/**
+ * Discord Packet
+ */
 export interface DiscordPacket {
+    /**
+     * opcode for the payload
+     */
     op: number;
+    /**
+     * event data
+     */
     d: any;
+    /**
+     * sequence number, used for resuming sessions and heartbeats
+     */
     s?: number;
+    /**
+     * the event name for this payload
+     */
     t?: string;
 }
 
+/**
+ * Lavalink Node Options
+ */
 export interface LavalinkNodeOptions {
+    /**
+     * The id of the LavalinkNode so Nodes are better organized
+     */
     id: string;
+    /**
+     * The host of the LavalinkNode, this could be a ip or domain.
+     */
     host: string;
+    /**
+     * The port of the LavalinkNode
+     */
     port?: number | string;
+    /**
+     * The password of the lavalink node
+     */
     password?: string;
+    /**
+     * The interval that the node will try to reconnect to lavalink at in milliseconds
+     */
     reconnectInterval?: number;
 }
 
+/**
+ * Lavalink Statistics
+ */
 export interface LavalinkStats {
+    /**
+     * The amount of players the node is handling
+     */
     players: number;
+    /**
+     * The amount of players that are playing something
+     */
     playingPlayers: number;
+    /**
+     * How long the LavalinkNode has been up for in milliseconds
+     */
     uptime: number;
+    /**
+     * memory information
+     */
     memory: {
+        /**
+         * The amount of memory that is free
+         */
         free: number;
+        /**
+         * the amount of memory that is used
+         */
         used: number;
+        /**
+         * The amount of allocated memory
+         */
         allocated: number;
+        /**
+         * The amount of reservable memory
+         */
         reservable: number;
     };
+    /**
+     * CPU Data
+     */
     cpu: {
+        /**
+         * The amount of cores the server has where lavalink is hosted
+         */
         cores: number;
+        /**
+         * System load of the server lavalink is on
+         */
         systemLoad: number;
+        /**
+         * The amount of load that lavalink is using
+         */
         lavalinkLoad: number;
     };
+    /**
+     * Frame statistics
+     */
     frameStats?: {
+        /**
+         * The amount of frames sent
+         */
         sent?: number;
+        /**
+         * The amount of frames nullified
+         */
         nulled?: number;
+        /**
+         * The amount of deficit frames
+         */
         deficit?: number;
     };
 }
 
+/**
+ * Queue Data
+ */
 export interface QueueData {
+    /**
+     * The data to actually send from the queue
+     */
     data: string;
+    /**
+     * The resolve function for the promise
+     */
     resolve: (value?: boolean | PromiseLike<boolean> | undefined) => void;
+    /**
+     * The reject function for the promise
+     */
     reject: (reason?: any) => void;
 }
 
+/**
+ * Websocket Close Event
+ */
 export interface WebsocketCloseEvent {
+    /**
+     * If the close was clean or not
+     */
     wasClean: boolean;
+    /**
+     * The code that was sent for the close
+     */
     code: number;
+    /**
+     * The reason of the closure
+     */
     reason: string;
+    /**
+     * The target
+     */
     target: WebSocket;
 }
