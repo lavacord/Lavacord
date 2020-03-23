ed397b315aba5166219c4b7857436b642e919a47
Jacz <itsmejacz@gmail.com>
2020-03-24 00:54:44 +1100

    Forgot to export Rest class and Util

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from "./lib/LavalinkNode";
 export * from "./lib/Player";
 export * from "./lib/Manager";
+export * from "./lib/Rest";
 export * from "./lib/Types";
+export * from "./lib/Util";
