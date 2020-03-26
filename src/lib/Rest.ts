1fa40e7fd0c36047162a8332da572198ffc0f5c6
Jacz <itsmejacz@gmail.com>
2020-03-27 01:15:06 +1100

    Make all methods Static

--- src/lib/Rest.ts
+++ src/lib/Rest.ts
@@ -15,7 +15,7 @@ export class Rest {
      * @param node The LavalinkNode
      * @param identifer The thing you want to load
      */
-    load(node: LavalinkNode, identifer: string): Promise<TrackResponse> {
+    static load(node: LavalinkNode, identifer: string): Promise<TrackResponse> {
         const params = new URLSearchParams();
         params.append("identifier", identifer);
 
@@ -27,10 +27,10 @@ export class Rest {
      * @param node The lavalink node
      * @param track the track(s) you want to decode
      */
-    public decode(node: LavalinkNode, track: string): Promise<TrackData>;
-    public decode(node: LavalinkNode, tracks: string[]): Promise<TrackData[]>;
-    public decode(node: LavalinkNode, tracks: string | string[]): Promise<TrackData | TrackData[]>;
-    public decode(node: LavalinkNode, tracks: string | string[]): Promise<TrackData | TrackData[]> {
+    static decode(node: LavalinkNode, track: string): Promise<TrackData>;
+    static decode(node: LavalinkNode, tracks: string[]): Promise<TrackData[]>;
+    static decode(node: LavalinkNode, tracks: string | string[]): Promise<TrackData | TrackData[]>;
+    static decode(node: LavalinkNode, tracks: string | string[]): Promise<TrackData | TrackData[]> {
         if (Array.isArray(tracks)) {
             return fetch(`http://${node.host}:${node.port}/decodetracks`, { method: "POST", body: JSON.stringify(tracks), headers: { Authorization: node.password } }).then(json);
         } else {
@@ -44,7 +44,7 @@ export class Rest {
      * A helper for /routeplanner/status
      * @param node The LavalinkNode
      */
-    public routePlannerStatus(node: LavalinkNode): Promise<RoutePlannerStatus> {
+    static routePlannerStatus(node: LavalinkNode): Promise<RoutePlannerStatus> {
         return fetch(`http://${node.host}:${node.port}/routeplanner/status`, { headers: { Authorization: node.password } }).then(json);
     }
 
@@ -53,7 +53,7 @@ export class Rest {
      * @param node The LavalinkNode
      * @param address the address you want to free, this is optional
      */
-    public routePlannerUnmark(node: LavalinkNode, address?: string): Promise<any> {
+    static routePlannerUnmark(node: LavalinkNode, address?: string): Promise<any> {
         if (address) {
             return fetch(`http://${node.host}:${node.port}/routeplanner/free/address`, { method: "POST", body: JSON.stringify({ address }), headers: { Authorization: node.password } }).then(json);
         }
