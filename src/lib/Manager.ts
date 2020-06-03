c57086d8cf66232deb231eead8222e1820840295
efindus <48939809+efindus@users.noreply.github.com>
2020-06-03 09:18:25 +0200

    Maybe this time?

--- src/lib/Manager.ts
+++ src/lib/Manager.ts
@@ -163,7 +163,7 @@ export class Manager extends EventEmitter {
         if (data.user_id !== this.user) return Promise.resolve(false);
 
         if (data.channel_id && data.guild_id) {
-            if (this.voiceStates && this.voiceStates.has(data.guild_id) && this.voiceStates.get(data.guild_id).channel_id) {
+            if (this && this.voiceStates && this.voiceStates.has(data.guild_id) && this.voiceStates.get(data.guild_id).channel_id) {
                 if (data.channel_id !== this.voiceStates.get(data.guild_id).channel_id) {
                     return this.voiceStates.set(data.guild_id, data);
                 }
